520b19d5fcbbfeb1fe63ac05f7ae31e4a163a939
Pawel Dubiel <contact@paweldubiel.com>
2017-05-15 22:23:35 +0200

    init commit

---
+++ Assets/Assets/Scripts/RotatingCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCamera : MonoBehaviour {
+
+	public float speed = 50;
+
+	void FixedUpdate() {
+		transform.RotateAround( Input.mousePosition, Input.mousePosition, speed * Mathf.Sin( Time.fixedDeltaTime) * Mathf.Cos( Time.fixedDeltaTime) );
+	}
+}
